a150eebcfa73a9a3c2298c47c5170602bdb94d43
Mteor <dblopes94@gmail.com>
2016-11-30 11:14:33 +0000

    Class Done

--- sliceParser.py
+++ sliceParser.py
@@ -1,1 +1,24 @@
-#Just A Comment
+class Slice:
+	name = ''
+	entryPoints = []
+	validations = []
+	sensitiveSynks = []
+
+	def __init__(self):
+		self.name = 'test'
+		self.entryPoints.append('EntryTest')
+		self.validations.append('ValidationTest')
+		self.sensitiveSynks.append('SynkTest')
+	
+	def getItems(self):
+		print("Name : " + self.name)
+		print("EntryPoints: ")
+		for x in range(len(self.entryPoints)):
+			print("Entry %d: %s" % (x,self.entryPoints[x]))
+		print("SensitiveSynks: ")
+		for x in range(len(self.sensitiveSynks)):
+			print("Synk %d: %s" % (x,self.sensitiveSynks[x]))
+
+
+test = Slice()
+test.getItems()
