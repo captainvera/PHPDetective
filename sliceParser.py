2722ee90ef247fdd8fc44e787f12738b76457b7a
Mteor <dblopes94@gmail.com>
2016-12-01 00:22:42 +0000

    Implementing sliceParser

--- sliceParser.py
+++ sliceParser.py
@@ -9,7 +9,7 @@ class Slice:
 		self.entryPoints.append('EntryTest')
 		self.validations.append('ValidationTest')
 		self.sensitiveSynks.append('SynkTest')
-	
+
 	def getItems(self):
 		print("Name : " + self.name)
 		print("EntryPoints: ")
@@ -19,6 +19,35 @@ class Slice:
 		for x in range(len(self.sensitiveSynks)):
 			print("Synk %d: %s" % (x,self.sensitiveSynks[x]))
 
-
+##################
 test = Slice()
 test.getItems()
+##################
+
+def fileParser():
+	#Insert FileName
+	fileName = "sqli_01.txt" #input("Enter file name: ")
+	print("File Name : %s" % fileName)
+
+	#Open fileName
+	fo = open(fileName, "r")
+	print("Is File Closed ? %s" % fo.closed)
+
+	#Read its content to a list
+	content = fo.readlines()
+	print(len(content))
+	for i in range(len(content)):
+		print(content[i])
+
+#AUX FUNTIONS
+def SQLInjection(content):
+	return 0
+
+def XSS(content):
+	return 0
+
+
+
+##################
+fileParser()
+##################
